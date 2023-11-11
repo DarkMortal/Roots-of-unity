6026cec5887dead63e9a410bb7d2b856cde3a1fb
DarkMortal <chodab74@gmail.com>
2023-11-11 16:16:56 +0530

    Final commit

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import matplotlib.pyplot as plt 
 import numpy as np
-from modules.magic import getRoots, load_css
+from modules.magic import getRoots, load_css, cleaned
 
 st.set_page_config(
     page_title = 'Roots of Unity',
@@ -17,28 +17,41 @@ st.markdown("### Check out this [Jupyter Notebook](%s)" % url)
 i = st.number_input("Enter i",min_value = 1, max_value = 10)
 r = st.number_input("Enter r",min_value = 1, max_value = 100)
 p = st.number_input("Enter precision",min_value = 1, max_value = 7, value = 3)
-gird_lines = st.checkbox("Show grid lines", value = True)
+
+# graph config
+is_circle = st.checkbox("Show circle", value = True)
+gird_lines = st.checkbox("Show grid lines", value = False)
 
 if st.button("Generate roots"):
-    roots = getRoots(i, float(r), p)
+    mag, roots = getRoots(i, float(r), p)
     st.caption("## Roots")
     for l in range(len(roots)):
-        root = str(roots[l])[1:-1]
+        root = cleaned(roots[l])
         if i > 1:
-            st.latex("\sqrt["+str(i)+"]{"+str(r)+"}\cos\\big(\\frac{"+str(2*l)+"\pi}{"+str(i)+"}\\big)+\sqrt["+str(i)+"]{"+str(r)+"}\sin\\big(\\frac{"+str(2*l)+"\pi}{"+str(i)+"}\\big)j = "+root)
+            if r == 1:
+                st.latex("\cos\\big(\\frac{"+str(2*l)+"\pi}{"+str(i)+"}\\big)+\sin\\big(\\frac{"+str(2*l)+"\pi}{"+str(i)+"}\\big)i = "+root)
+            else:
+                st.latex("\sqrt["+str(i)+"]{"+str(r)+"}\cos\\big(\\frac{"+str(2*l)+"\pi}{"+str(i)+"}\\big)+\sqrt["+str(i)+"]{"+str(r)+"}\sin\\big(\\frac{"+str(2*l)+"\pi}{"+str(i)+"}\\big)i = "+root)
         else:
-            st.latex("\sqrt["+str(i)+"]{"+str(r)+"}\cos("+str(2*l)+"\pi)+\sqrt["+str(i)+"]{"+str(r)+"}\sin("+str(2*l)+"\pi)j = "+root)
+            if r == 1:
+                st.latex("\cos("+str(2*l)+"\pi)+\sin("+str(2*l)+"\pi)i = "+root)
+            else:
+                st.latex("\sqrt["+str(i)+"]{"+str(r)+"}\cos("+str(2*l)+"\pi)+\sqrt["+str(i)+"]{"+str(r)+"}\sin("+str(2*l)+"\pi)i = "+root)
         
     roots = np.array(roots)
     x = roots.real 
-    # extract imaginary part using numpy array 
     y = roots.imag 
 
-    # plot the complex numbers 
-    plt.plot(x, y, 'g*')
     plt.ylabel('Imaginary') 
     plt.xlabel('Real')
     plt.grid(gird_lines)
+    plt.gca().set_aspect(1)
+
+    if is_circle:
+        circle = plt.Circle((0.0, 0.0), mag , fill = False )
+        plt.gca().add_patch(circle)
+    plt.plot(x, y, 'o')
+
     st.caption("## Graph")
     st.pyplot(plt)
 
--- modules/magic.py
+++ modules/magic.py
@@ -4,16 +4,26 @@ from modules.recursive import sine as sin, cosine as cos, to_radians, root
 def getRoots(i:int, r:float, precision = 3) -> []:
     div = 360.0 / i
     
+    mag = root(r, i)
     roots = []
+
     for n in range(i):
-        mag = root(r, i)
         angle = to_radians(div * n)
         real = mag * cos(angle)
         img = mag * sin(angle)
         roots.append(complex(round(real, precision),round(img, precision)))
 
-    return roots
+    return (mag, roots)
 
 def load_css(filepath = "./styles/styles.css"):
     with open(filepath) as file:
         md(f'<style>{file.read()}</style>', unsafe_allow_html=True)
+
+def cleaned(c:complex) -> str:
+    if c.imag == 0 and c.real == 0:
+        return "0"
+    if c.imag == 0:
+        return str(c.real)
+    if c.real == 0:
+        return f"{str(c.imag)}i"
+    return str(c).replace('(','').replace(')','').replace('j','i')
